1e62b8e3beb3f1b2d6d28787b82fe0692a2674d7
Stefan Dombek <dombek@ub.uni-leipzig.de>
2022-11-29 14:08:49 +0100

    Create pg_catalog_extraction_derivedTables.sql

---
+++ sql/derived_tables/pg_catalog_extraction_derivedTables.sql
@@ -0,0 +1,19 @@
+-- The report lists the attributes of the derived tables from folio-analytics and links to the source in the repository.
+SELECT
+    columns.table_name AS "table",
+    columns.ordinal_position AS "attribute #", 
+    columns.column_name AS "attribute",
+    columns.udt_name AS "datatype",
+    pg_description.description,
+    'https://github.com/folio-org/folio-analytics/tree/main/sql_metadb/derived_tables' || '/' || columns.table_name || '.sql' AS "Link"
+FROM
+    pg_catalog.pg_statio_all_tables
+    INNER JOIN pg_catalog.pg_description ON pg_description.objoid = pg_statio_all_tables.relid
+    INNER JOIN information_schema.columns ON pg_description.objsubid = columns.ordinal_position 
+        AND columns.table_schema = pg_statio_all_tables.schemaname 
+        AND columns.table_name = pg_statio_all_tables.relname
+WHERE 
+	columns.table_schema = 'folio_derived'
+ORDER BY 
+	columns.table_name,
+	columns.ordinal_position
