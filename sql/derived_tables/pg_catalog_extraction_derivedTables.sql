8baaff05e6b9e11b53334e7ad6981741aba7c4f9
Stefan Dombek <dombek@ub.uni-leipzig.de>
2023-02-16 15:25:03 +0100

    Update pg_catalog_extraction_derivedTables.sql
    Closes #4 , all derived tables are now displayed.

--- sql/derived_tables/pg_catalog_extraction_derivedTables.sql
+++ sql/derived_tables/pg_catalog_extraction_derivedTables.sql
@@ -1,15 +1,13 @@
--- The report lists the attributes of the derived tables from folio-analytics and links to the source in the repository.
 SELECT
     columns.table_name AS "table",
     columns.ordinal_position AS "attribute #", 
     columns.column_name AS "attribute",
     columns.udt_name AS "datatype",
-    pg_description.description,
-    'https://github.com/folio-org/folio-analytics/tree/main/sql_metadb/derived_tables' || '/' || columns.table_name || '.sql' AS "Link"
+    COALESCE(pg_description.description, '') AS description
 FROM
     pg_catalog.pg_statio_all_tables
-    INNER JOIN pg_catalog.pg_description ON pg_description.objoid = pg_statio_all_tables.relid
-    INNER JOIN information_schema.columns ON pg_description.objsubid = columns.ordinal_position 
+    LEFT JOIN pg_catalog.pg_description ON pg_description.objoid = pg_statio_all_tables.relid
+    RIGHT JOIN information_schema.columns ON pg_description.objsubid = columns.ordinal_position 
         AND columns.table_schema = pg_statio_all_tables.schemaname 
         AND columns.table_name = pg_statio_all_tables.relname
 WHERE 
