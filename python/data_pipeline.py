409caf2254e5afc65e6f8f701ce3442ff4859879
Stefan Dombek <dombek@ub.uni-leipzig.de>
2023-02-16 15:32:15 +0100

    Update data_pipeline.py
    Bugfix in SQL #4

--- python/data_pipeline.py
+++ python/data_pipeline.py
@@ -83,22 +83,22 @@ try:
 
     # configure the database query and execute the database query
     cur.execute("""
-        SELECT 
-            columns.table_name AS "table", 
+        SELECT
+            columns.table_name AS "table",
             columns.ordinal_position AS "attribute #", 
-            columns.column_name AS "attribute", 
-            columns.udt_name AS "datatype", 
-            pg_description.description 
-        FROM 
-            pg_catalog.pg_statio_all_tables 
-            INNER JOIN pg_catalog.pg_description ON pg_description.objoid = pg_statio_all_tables.relid 
-            INNER JOIN information_schema.columns ON pg_description.objsubid = columns.ordinal_position 
+            columns.column_name AS "attribute",
+            columns.udt_name AS "datatype",
+            COALESCE(pg_description.description, '') AS description
+        FROM
+            pg_catalog.pg_statio_all_tables
+            LEFT JOIN pg_catalog.pg_description ON pg_description.objoid = pg_statio_all_tables.relid
+            RIGHT JOIN information_schema.columns ON pg_description.objsubid = columns.ordinal_position 
                 AND columns.table_schema = pg_statio_all_tables.schemaname 
-                AND columns.table_name = pg_statio_all_tables.relname 
+                AND columns.table_name = pg_statio_all_tables.relname
         WHERE 
-            columns.table_schema = 'folio_derived' 
+            columns.table_schema = 'folio_derived'
         ORDER BY 
-            columns.table_name, 
+            columns.table_name,
             columns.ordinal_position
     """)
 
